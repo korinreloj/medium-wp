a41a31f1b15095d6b3b31a106dbba1fd9ab6a5b9
Josh <josh@cerbito.com>
2018-08-06 16:05:19 +0800

    BE: Generate Necessary Files for Theming

---
+++ wp-content/themes/medium-wp/footer.php
@@ -0,0 +1,7 @@
+  <footer>
+    HELLO, I'm footer
+  </footer>
+
+  <?php /* <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/dist/css/styles.css"></script> */ ?>
+</body>
+</html>
---
+++ wp-content/themes/medium-wp/front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+  <section class="featured-content">
+    
+  </section>
+
+<?php get_footer(); ?>
