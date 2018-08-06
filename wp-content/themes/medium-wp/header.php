27306f92718172e0375a6cdb42cc74e077283970
Josh <josh@cerbito.com>
2018-08-06 16:06:21 +0800

    ✨ BE: Generate Necessary Files for Theming
    commit a41a31f1b15095d6b3b31a106dbba1fd9ab6a5b9
    Author: Josh <josh@cerbito.com>
    Date:   Mon Aug 6 16:05:19 2018 +0800
    
        BE: Generate Necessary Files for Theming

---
+++ wp-content/themes/medium-wp/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/css/styles.css">
+  </head>
+  <body>
+    
+  <header>
+    HELLO, I'm header
+  </header>
