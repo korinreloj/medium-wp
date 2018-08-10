43d769dc222928751b6439485c551573e116f861
Samantha Dacanay <sathdacanay@gmail.com>
2018-08-10 17:38:43 +0800

    🍱 Updated front-page

--- wp-content/themes/medium-wp/front-page.php
+++ wp-content/themes/medium-wp/front-page.php
@@ -1,7 +1,11 @@
-<?php get_header(); ?>
+<?php get_header() ?>
 
-  <section class="featured-content">
-    
-  </section>
+<?php @include 'category-nav.php' ?>
+<?php @include 'featured-content.php' ?>
+
+<div class="content">
+	<?php @include 'main-feed.php' ?>
+	<?php @include 'feed-sidebar.php' ?>
+</div>
 
 <?php get_footer(); ?>
