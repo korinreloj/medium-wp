fddfc5840d42de1261996c9509c7add656ba0088
Samantha Dacanay <sathdacanay@gmail.com>
2018-08-15 10:44:42 +0800

    🍱 FE: Feed-Sidebar

--- wp-content/themes/medium-wp/front-page.php
+++ wp-content/themes/medium-wp/front-page.php
@@ -1,6 +1,14 @@
-<?php
- get_header();
- @include 'category-nav.php';
- @include 'featured-content.php';
-?>
+<?php get_header() ?>
+
+<?php @include 'category-nav.php' ?>
+<?php @include 'featured-content.php' ?>
+<?php #@include 'hero.php' ?>
+
+<div class="content">
+	<?php #@include 'main-feed.php' ?>
+	<?php #@include 'feed-sidebar.php' ?>
+</div>
+
+<?php @include 'article.php' ?>
+
 <?php get_footer(); ?>
