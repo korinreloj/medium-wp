e6446b9523474c497abd71955d82dc20acce0c60
korinreloj <42025708+korinreloj@users.noreply.github.com>
2018-08-20 16:13:44 +0800

    🍱 FE: Featured Content (#48)
    * 🍱 FE: Featured Content
    
    * 🍱 Updated styling and footer details

---
+++ wp-content/themes/medium-wp/featured-content.php
@@ -0,0 +1,138 @@
+<div class="featured-content">
+	<section class="featured-content__cards">
+
+    <!-- large card -->
+    <article class="featured-content__cards__large">
+    	<figure class="featured-content__cards__large__image">
+    		<img src="https://cdn-images-1.medium.com/focal/832/302/45/56/1*bnLe5b0ELyYs_rTY5kvMng.jpeg">
+    	</figure>
+    	<div class="featured-content__cards__large__details">
+        <a href="#">
+      		<h2 class="large-title">
+      			Words of Wisdom From the American Jazz and Negro League Baseball Museum Host
+      		</h2>
+          <p>
+            Ronnie Matloch loves talking history almost as much as he loves people
+          </p>
+        </a>
+        <footer>
+          <div class="footer-content">
+            <p><a href="#">Josh S. Rose</a> in <a href="#">America at Work</a></p>
+            <p class="add-details">Apr 24, 2017 &bull; <i class="fa fa-star"></i> 7 min read</p>
+          </div>
+          <div class="bookmark-container">
+            <i class="fa fa-bookmark-o"></i>
+          </div>
+        </footer>
+      </div>
+    </article>
+
+
+    <!-- small cards -->
+    <div class="featured-content__smallContainer">
+      <article class="featured-content__cards__small">
+        <div class="featured-content__cards__small__details">
+          <a href="#">
+            <h3 class ="small-title">
+              What I Learned from Gods Who Don’t Exist
+            </h3>
+            <p class="subtitle">
+              Ronnie Matloch loves talking history almost as much as he loves people
+            </p>
+          </a>
+          <footer>
+            <div class="footer-content">
+              <p><a href="#">Josh S. Rose</a></p>
+              <p class="add-details">Apr 24, 2017 &bull; <i class="fa fa-star"></i> 7 min read</p>
+            </div>
+            <div class="bookmark-container">
+              <i class="fa fa-bookmark-o"></i>
+            </div>
+          </footer>
+      	</div>
+
+        <figure class="featured-content__cards__small__image">
+          <img src="https://cdn-images-1.medium.com/focal/100/100/50/41/1*d7NgHQo_Zp46_Ss0_8qgyw.gif">
+        </figure>
+      </article>
+
+      <article class="featured-content__cards__small">
+        <div class="featured-content__cards__small__details">
+          <a href="#">
+            <h3 class ="small-title without-sub">
+              What I Learned from Gods Who Don’t Exist
+            </h3>
+          </a>
+          <footer>
+            <div class="footer-content">
+              <p><a href="#">Josh S. Rose</a> in <a href="#">America at Work</a></p>
+              <p class="add-details">Apr 24, 2017 &bull; <i class="fa fa-star"></i> 7 min read</p>
+            </div>
+            <div class="bookmark-container">
+              <i class="fa fa-bookmark-o"></i>
+            </div>
+          </footer>
+        </div>
+
+        <figure class="featured-content__cards__small__image">
+          <img src="https://cdn-images-1.medium.com/focal/100/100/51/63/0*XF3aw52-Rq_42nR2">
+        </figure>
+      </article>
+
+      <article class="featured-content__cards__small">
+        <div class="featured-content__cards__small__details">
+          <a href="#">
+            <h3 class ="small-title without-sub">
+              What I Learned from Gods Who Don’t Exist
+            </h3>
+          </a>
+          <footer>
+            <div class="footer-content">
+              <p><a href="#">Josh S. Rose</a> in <a href="#">America at Work</a></p>
+              <p class="add-details">Apr 24, 2017 &bull; <i class="fa fa-star"></i> 7 min read</p>
+            </div>
+            <div class="bookmark-container">
+              <i class="fa fa-bookmark-o"></i>
+            </div>
+          </footer>
+        </div>
+
+        <figure class="featured-content__cards__small__image">
+          <img src="https://cdn-images-1.medium.com/focal/100/100/51/63/0*XF3aw52-Rq_42nR2">
+        </figure>
+      </article>
+    </div>
+
+    <!-- medium card -->
+    <div class="featured-content__cards__mediumContainer">
+    	<article class="featured-content__cards__medium">
+      <figure class="featured-content__cards__medium__image">
+        <img src="https://cdn-images-1.medium.com/focal/832/302/45/56/1*bnLe5b0ELyYs_rTY5kvMng.jpeg">
+      </figure>
+      <div class="featured-content__cards__medium__details">
+        <a href="#">
+          <h2 class="medium-title">
+            Words of Wisdom From the American Jazz and Negro League Baseball Museum Host
+          </h2>
+          <p class="subtitle">
+            Ronnie Matloch loves talking history almost as much as he loves people
+          </p>
+        </a>
+        <footer>
+          <div class="footer-content">
+            <p><a href="#">Josh S. Rose</a> in <a href="#">America at Work</a></p>
+            <p class="add-details">Apr 24, 2017 &bull; <i class="fa fa-star"></i> 7 min read</p>
+          </div>
+        </footer>
+
+      </div>
+    </article>
+
+    </div>
+  </section>
+
+  <!-- featured link below featured content -->
+  <div class="featured-link">
+    <a href="#">See All Featured ></a>
+  </div>
+</div>
