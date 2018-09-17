4db8c7d55da370cbfca0411f14bb01c72e17e2ca
Kim Cornell <khennahcornell@gmail.com>
2018-09-17 15:08:51 +0800

    🍱BE: Updated featured-content.php

--- wp-content/themes/medium-wp/front-page.php
+++ wp-content/themes/medium-wp/front-page.php
@@ -13,16 +13,5 @@
 ?>
 </section>
 
-<!-- <section class="featured-content">
-<?php
-  $featured_articles = get_field('featured_articles');
-  foreach($featured_articles as $article):
-?>
-  <h1>
-    <a href="<?php echo $article->guid; ?>"><?php echo $article->post_title; ?></a>
-  </h1>
-<?php endforeach; ?>
-</section>
-
 <?php get_footer(); ?>
 
