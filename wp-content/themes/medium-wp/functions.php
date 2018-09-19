0963ec7a3613310d6583a3ddc87675c560fa1e35
Corinne Reloj <relojcorinne@gmail.com>
2018-09-19 15:27:51 +0800

    🍱 Updated styling of Hero Image and Author Info

--- wp-content/themes/medium-wp/functions.php
+++ wp-content/themes/medium-wp/functions.php
@@ -1,7 +1,11 @@
 <?php
 
-include_once('advanced-custom-fields/acf.php');
 
-include_once('ACF/article-fields.php');
+// include_once('advanced-custom-fields-pro/acf.php');
 
-include_once('ACF/breaker.php');
+// include_once('ACF/article-fields.php');
+
+// include_once('ACF/breaker.php');
+
+include_once('ACF/flexible-content.php');
+include_once('ACF/main_feed.php');
